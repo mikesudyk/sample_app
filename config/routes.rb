fb85ca29e3d53829a1d86a148fd7b4ba000ae175
mikesudyk <mikesudyk@gmail.com>
2011-08-12 10:58:02 -0400

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 SampleApp::Application.routes.draw do
 
- match '/contact', :to => 'pages#contact'
- match '/about', :to => 'pages#about'
- match '/help', :to => 'pages#help'
- 
- root :to => 'pages#home'
+  get "users/new"
+
+  match '/signup', :to => 'users#new'
+
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+
+  root :to => 'pages#home'
 
 end
