e1e1a2982bea619395270fee64f1816b83550c57
mikesudyk <mikesudyk@gmail.com>
2011-09-05 08:52:14 -0400

    Done with sign in

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Users" do
+
  describe "signup" do
   describe "failure" do
    it "should not make a new user" do
@@ -30,4 +31,7 @@ describe "Users" do
    end
   end  
  end
+
+
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,13 +8,11 @@ require 'rspec/rails'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
+  
+  def test_sign_in(user)
+   controller.sign_in(user)
+  end
+
   config.mock_with :rspec
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
