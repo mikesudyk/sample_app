81693aeae55d4660e68bbd35c544bd54b0218855
mikesudyk <mikesudyk@gmail.com>
2011-10-27 13:19:32 -0400

    added user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,13 @@ class User < ActiveRecord::Base
  attr_accessible :name, :email, :password, :password_confirmation
 
  has_many :microposts, :dependent => :destroy
+ has_many :relationships, :foreign_key => "follower_id",
+			  :dependent => :destroy
+ has_many :following, :through => :relationships, :source => :followed
+ has_many :reverse_relationships, :foreign_key => "followed_id",
+                                  :class_name => "Relationship",
+                                  :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
 
  email_regrex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -35,21 +42,32 @@ class User < ActiveRecord::Base
     encrypted_password == encrypt(submitted_password)
   end
 
- def self.authenticate(email, submitted_password)
-  user = find_by_email(email)
-  return nil if user.nil?
-  return user if user.has_password?(submitted_password)
- end
+  def self.authenticate(email, submitted_password)
+   user = find_by_email(email)
+   return nil if user.nil?
+   return user if user.has_password?(submitted_password)
+  end
 
- def self.authenticate_with_salt(id, cookie_salt)
-  user = find_by_id(id)
-  (user && user.salt == cookie_salt) ? user : nil
- end
+  def self.authenticate_with_salt(id, cookie_salt)
+   user = find_by_id(id)
+   (user && user.salt == cookie_salt) ? user : nil
+  end
 
- def feed
-  # This is preliminary
-  Micropost.where("user_id = ?", id)
- end
+  def following?(followed)
+   relationships.find_by_followed_id(followed)
+  end
+
+  def follow!(followed)
+   relationships.create!(:followed_id => followed.id)
+  end
+
+  def unfollow!(followed)
+   relationships.find_by_followed_id(followed).destroy
+  end
+
+  def feed
+    Micropost.from_users_followed_by(self)
+  end
 
   private
 
